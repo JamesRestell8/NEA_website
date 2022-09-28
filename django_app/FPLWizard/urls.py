6f48afb36638e863ff816c0c3606d039f3ff9bc6
James Restell <james.restell@icloud.com>
2022-09-28 19:26:50 +0100

    website now error free - going to home/ will display current datetime

--- django_app/FPLWizard/urls.py
+++ django_app/FPLWizard/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from . import mainMenu
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(mainMenu.urls))
+    path('home/', include('mainMenu.urls'))
 ]
