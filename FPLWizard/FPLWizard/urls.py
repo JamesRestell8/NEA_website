fe8e9d2d2e9517350b2f539efc7580f05ca5c893
James Restell <james.restell@icloud.com>
2022-10-09 10:33:34 +0100

    Changed default home page. Added static code for formatting and started on a mock-up nav-bar

--- FPLWizard/FPLWizard/urls.py
+++ FPLWizard/FPLWizard/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('webApp/', include('webApp.urls')),
+    path('', include('webApp.urls'))
 ]
--- FPLWizard/webApp/views.py
+++ FPLWizard/webApp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views import generic
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    template = loader.get_template("webApp/index.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
