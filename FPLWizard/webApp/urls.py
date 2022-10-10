cc1b099d21c46f75a0c7d6b0bd10d7557f5551eb
James Restell <james.restell@icloud.com>
2022-10-10 16:54:25 +0100

    Fully functioning navbar, need to improve the design and remove the all players page becuase it isn't necessary for project

--- FPLWizard/webApp/urls.py
+++ FPLWizard/webApp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('allPlayers/', views.allPlayers, name='allPlayers'),
+    path('MyFPL/', views.myFPL, name="MyFPL"),
+    path('Positions/', views.positions, name='positions')
 ]
--- FPLWizard/webApp/views.py
+++ FPLWizard/webApp/views.py
@@ -5,7 +5,32 @@ from django.template import loader
 
 # Create your views here.
 
+# Home/Welcome screen
 def index(request):
     template = loader.get_template("webApp/index.html")
     context = {}
+    return HttpResponse(template.render(context, request))
+
+
+def allPlayers(request):
+    template = loader.get_template("webApp/allPlayers.html")
+    context = {
+        'pageName': "All Players"
+        }
+    return HttpResponse(template.render(context, request))
+
+
+def positions(request):
+    template = loader.get_template("webApp/positions.html")
+    context = {
+        'pageName': 'Positions'
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def myFPL(request):
+    template = loader.get_template("webApp/myFPL.html")
+    context = {
+        'pageName': "My FPL"
+    }
     return HttpResponse(template.render(context, request))
