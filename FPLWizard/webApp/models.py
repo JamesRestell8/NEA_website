2f413aa7a0f4979b196921da17c2bd10ea2b534a
James Restell <james.restell@icloud.com>
2022-11-13 17:03:57 +0000

    Backend now built but not populated. Added timeManager table for knowing when to update the database. Password field on MyFPL still needs securing. Get proper .png for favicon. Knapsack algorithm is next big thing after getting the database working.

--- FPLWizard/webApp/models.py
+++ FPLWizard/webApp/models.py
@@ -25,6 +25,9 @@ class FPLAPIStatsGameweek(models.Model):
     fpl_creativity = models.FloatField(verbose_name='Creativity')
     fpl_ppg = models.FloatField(verbose_name='Points Per Game')
 
+    def __str__(self):
+        return self.fpl_player_name
+
 
 # A table to store player data retrieved from the understat API. Similar function to the table above
 class UnderstatAPIStatsGameweek(models.Model):
@@ -46,22 +49,26 @@ class Gameweek(models.Model):
     gameweekNumber = models.IntegerField(primary_key=True, verbose_name='Gameweek No.')
     endDeadline = models.DateTimeField()
 
+    def __str__(self):
+        return "Gameweek " + str(self.gameweekNumber)
+
 class Team(models.Model):
     teamID = models.IntegerField(primary_key=True)
     teamName = models.CharField(max_length=100)
     leaguePos = models.IntegerField(verbose_name='League Position', validators=[MinValueValidator(0),MaxValueValidator(21)])
 
+    def __str__(self):
+        return self.teamName
 
 class Fixture(models.Model):
-    pass
-    #fixtureID = models.IntegerField(primary_key=True)
-    #homeTeamID = models.ForeignKey(Team, verbose_name='Home Team ID', on_delete=models.CASCADE)
-    #awayTeamID = models.ForeignKey(Team, verbose_name='Away Team ID', on_delete=models.CASCADE)
-    #homeTeamStrength = models.IntegerField(verbose_name="Home Team Strength")
-    #awayTeamStrength = models.IntegerField(verbose_name="Away Team Strength")
-    #homeTeamGoals = models.IntegerField(verbose_name="Home Team Goals")
-    #awayTeamGoals = models.IntegerField(verbose_name="Away Team Goals")
-    #gameweekNumber = models.ForeignKey(Gameweek, verbose_name='Gameweek', on_delete=models.CASCADE)
+    fixtureID = models.IntegerField(primary_key=True)
+    homeTeamID = models.ForeignKey(Team, verbose_name='Home Team ID', on_delete=models.CASCADE, related_name="homeTeamID")
+    awayTeamID = models.ForeignKey(Team, verbose_name='Away Team ID', on_delete=models.CASCADE, related_name="awayTeamID")
+    homeTeamStrength = models.IntegerField(verbose_name="Home Team Strength")
+    awayTeamStrength = models.IntegerField(verbose_name="Away Team Strength")
+    homeTeamGoals = models.IntegerField(verbose_name="Home Team Goals")
+    awayTeamGoals = models.IntegerField(verbose_name="Away Team Goals")
+    gameweekNumber = models.ForeignKey(Gameweek, verbose_name='Gameweek', on_delete=models.CASCADE)
 
 
 class APIIDDictionary(models.Model):
@@ -79,6 +86,8 @@ class PlayerTeam(models.Model):
     # on_delete = models.CASCADE means if the foreign object is deleted, the child object is also deleted
     teamID = models.ForeignKey(Team, verbose_name='Team ID', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.playerID + " - " + self.teamID
 
 class TeamFixture(models.Model):
     # using same composite foreign primary key method as in PlayerTeam table
@@ -96,4 +105,9 @@ class XPGameweek(models.Model):
     playerID = models.ForeignKey(APIIDDictionary, verbose_name='Player ID', on_delete=models.CASCADE)
 
     xP = models.FloatField(verbose_name='Expected Points')
-    formCoefficient = models.FloatField(verbose_name='Form coefficient') 
+    formCoefficient = models.FloatField(verbose_name='Form coefficient')
+
+
+class timeManager(models.Model):
+    timeID = models.IntegerField(primary_key=True)
+    lastModified = models.DateTimeField(auto_now_add=True)
