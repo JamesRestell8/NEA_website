ec5618dcb317f02028645c31ce4c907eaf538a76
James Restell <james.restell@icloud.com>
2023-01-22 14:00:19 +0000

    Removed unused database tables.

--- FPLWizard/webApp/models.py
+++ FPLWizard/webApp/models.py
@@ -57,8 +57,6 @@ class Fixture(models.Model):
     fixtureID = models.IntegerField(primary_key=True)
     homeTeamID = models.IntegerField(verbose_name='Home Team ID')
     awayTeamID = models.IntegerField(verbose_name='Away Team ID')
-    homeTeamStrength = models.IntegerField(verbose_name="Home Team Strength")
-    awayTeamStrength = models.IntegerField(verbose_name="Away Team Strength")
     homeTeamGoals = models.IntegerField(verbose_name="Home Team Goals")
     awayTeamGoals = models.IntegerField(verbose_name="Away Team Goals")
     gameweekNumber = models.IntegerField(verbose_name='Gameweek')
@@ -84,21 +82,3 @@ class PlayerTeamAndPosition(models.Model):
 
     def __str__(self):
         return self.playerID + " - " + self.teamID
-
-class TeamFixture(models.Model):
-    # using same composite foreign primary key method as in PlayerTeam table
-    # by defualt, Django names the ID field in a table "TableName_id"
-    num = models.IntegerField(primary_key=True)
-    fixtureID = models.ForeignKey(Fixture, verbose_name='Fixture ID', on_delete=models.CASCADE)
-    teamID = models.ForeignKey(Team, verbose_name='Team ID', on_delete=models.CASCADE)
-
-
-class XPGameweek(models.Model):
-    # using same composite foreign primary key method as in PlayerTeam table
-    num = models.IntegerField(primary_key=True)
-
-    gameweekNumber = models.IntegerField(verbose_name='Gameweek')
-    playerID = models.ForeignKey(APIIDDictionary, verbose_name='Player ID', on_delete=models.CASCADE)
-
-    xP = models.FloatField(verbose_name='Expected Points')
-    formCoefficient = models.FloatField(verbose_name='Form coefficient')
